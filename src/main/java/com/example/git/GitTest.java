ed9582d51ffbe4ce5c1198c3a3574f35da7739c5
LiKaiTong <1322858735@qq.com>
2023-05-22 16:28:53 +0800

    idea first commit

---
+++ src/main/java/com/example/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.example.git;
+
+/**
+ * @author LKT
+ * @create 2023-05-22 16:23
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
